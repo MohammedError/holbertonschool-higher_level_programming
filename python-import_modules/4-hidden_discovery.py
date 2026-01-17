22d4cfae5be5485109a5db4fb7711a281a4c1a00
MohammedError <mohammed.secure@outlook.sa>
2026-01-17 14:05:07 +0000

    Task 4: Who are you

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,7 @@
 if __name__ == "__main__":
     import hidden_4
 
-    for name in sorted(dir(hidden_4)):
-        if not name.startswith("__"):
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
             print(name)
