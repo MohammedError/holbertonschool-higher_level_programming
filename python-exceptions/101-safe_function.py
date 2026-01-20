6b3b01686e4de8757ba2591e5c1dd5d0082c4f86
MohammedError <mohammed.secure@outlook.sa>
2026-01-20 09:53:03 +0300

    Task 8: Safe function

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-import sys
-
 def safe_function(fct, *args):
     try:
         return fct(*args)
     except Exception as e:
-        sys.stderr.write("Exception: {}\n".format(e))
+        import sys
+        print("Exception: {}".format(e), file=sys.stderr)
         return None
