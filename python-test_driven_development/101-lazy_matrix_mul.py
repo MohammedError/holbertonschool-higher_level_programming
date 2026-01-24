84e4b4b82f1374d4acc7fbe0da4773eb2e912d51
MohammedError <mohammed.secure@outlook.sa>
2026-01-24 20:46:41 +0300

    Force exact error messages for Task 7 checker

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""Matrix multiplication using NumPy."""
 import numpy as np
 
-
 def lazy_matrix_mul(m_a, m_b):
-    """Return matrix multiplication."""
     return np.matmul(m_a, m_b)
