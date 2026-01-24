c9db295e79294bcea99f3d2d6e8cfc0f932a15ef
MohammedError <mohammed.secure@outlook.sa>
2026-01-24 14:11:56 +0300

    Task 7: Lazy matrix multiplication

---
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This is the "101-lazy_matrix_mul" module.
+
+The 101-lazy_matrix_mul module supplies one function, lazy_matrix_mul(m_a, m_b).
+"""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    Multiplies two matrices using the NumPy module.
+
+    Args:
+        m_a: The first matrix.
+        m_b: The second matrix.
+
+    Returns:
+        The resulting matrix from the multiplication.
+    """
+    return np.matmul(m_a, m_b)
