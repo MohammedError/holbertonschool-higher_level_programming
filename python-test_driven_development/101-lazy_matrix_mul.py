2833299fe836710acc5031ec67861de066058afe
MohammedError <mohammed.secure@outlook.sa>
2026-01-24 20:54:20 +0300

    Task 7: Lazy matrix multiplication with full validation

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,14 +1,43 @@
 #!/usr/bin/python3
+"""
+Module that provides a function to multiply two matrices
+using NumPy after validating them.
+"""
 import numpy as np
 
+
 def lazy_matrix_mul(m_a, m_b):
-    try:
-        return np.matmul(m_a, m_b)
-    except TypeError as e:
-        # Force old Checker message
-        if "matmul" in str(e) or "not supported" in str(e):
-            raise TypeError("Scalar operands are not allowed, use '*' instead")
-        else:
-            raise
-    except ValueError as e:
-        raise
+    """Multiplies two matrices using NumPy"""
+
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+
+    if m_a == [] or m_a == [[]]:
+        raise ValueError("m_a can't be empty")
+    if m_b == [] or m_b == [[]]:
+        raise ValueError("m_b can't be empty")
+
+    if not all(isinstance(row, list) for row in m_a):
+        raise TypeError("m_a must be a list of lists")
+    if not all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_b must be a list of lists")
+
+    if not all(isinstance(x, (int, float)) for row in m_a for x in row):
+        raise TypeError("m_a should contain only integers or floats")
+    if not all(isinstance(x, (int, float)) for row in m_b for x in row):
+        raise TypeError("m_b should contain only integers or floats")
+
+    row_len_a = len(m_a[0])
+    if not all(len(row) == row_len_a for row in m_a):
+        raise TypeError("each row of m_a must be of the same size")
+
+    row_len_b = len(m_b[0])
+    if not all(len(row) == row_len_b for row in m_b):
+        raise TypeError("each row of m_b must be of the same size")
+
+    if row_len_a != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+
+    return np.matmul(m_a, m_b)
