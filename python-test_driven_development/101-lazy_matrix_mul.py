536a29d483a126e2927d5bf81d4709d1c24f515b
MohammedError <mohammed.secure@outlook.sa>
2026-01-24 20:38:50 +0300

    Fix Task 7: Lazy matrix multiplication

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python3
-"""
-Module to multiply 2 matrices using NumPy
-"""
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """
-    Multiplies two matrices using the NumPy module
-    Args:
-        m_a: first matrix
-        m_b: second matrix
-    Returns:
-        return m_a * m_b
-    """
     return np.matmul(m_a, m_b)
