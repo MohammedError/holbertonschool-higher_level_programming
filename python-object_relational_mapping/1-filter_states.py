8d90feb129e312e55035be7f7bd450d5ab9e4b35
MohammedError <mohammed.secure@outlook.sa>
2026-02-28 02:31:48 +0300

    Fix Task 1 - Correct output for lowercase 'n'

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Lists all states with a name starting with N from the database hbtn_0e_0_usa."""
+"""Lists all states with a name starting with n from the database hbtn_0e_0_usa."""
 
 import MySQLdb
 import sys
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         charset="utf8"
     )
     cur = db.cursor()
-    cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT id, name FROM states WHERE name LIKE 'n%' ORDER BY id ASC")
     for row in cur.fetchall():
         print(row)
     cur.close()
