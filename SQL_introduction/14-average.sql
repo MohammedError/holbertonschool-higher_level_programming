883444c1ccfa137288611cebed91a4949a646d86
MohammedError <mohammed.secure@outlook.sa>
2026-02-28 01:25:11 +0300

    Average score task

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
--- Computes the score average of all records in the table second_table
-SELECT score, AVG(score) AS average FROM second_table GROUP BY score;
+-- computes the score average of all records in the table second_table
+SELECT AVG(score) AS average FROM second_table;
