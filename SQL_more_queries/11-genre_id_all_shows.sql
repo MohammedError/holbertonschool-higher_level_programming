278074a20d59a54bf8f3400fc473194f1dfa900f
MohammedError <mohammed.secure@outlook.sa>
2026-02-28 01:53:37 +0300

    Add Task 11

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- LISTS ALL SHOWS AND THEIR GENRE ID (IF ANY)
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
