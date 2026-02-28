1246e1b61240c2ba65cc14bf6bf9b65248959063
MohammedError <mohammed.secure@outlook.sa>
2026-02-28 23:36:20 +0300

    Fix task 2 with documentation, case-sensitive search, and pycodestyle

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
-"""
-Lists all states from the database hbtn_0e_0_usa that match the user input.
-"""
+"""Lists all states from the database hbtn_0e_0_usa that match the user input."""
+
 import MySQLdb
 import sys
 
-if name == "main":
-db = MySQLdb.connect(
-host="localhost",
-port=3306,
-user=sys.argv[1],
-passwd=sys.argv[2],
-db=sys.argv[3],
-charset="utf8"
-)
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
 
-cur = db.cursor()
-query = "SELECT * FROM states WHERE BINARY name = '{}' ".format(sys.argv[4])
-query += "ORDER BY id ASC"
-cur.execute(query)
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE BINARY name = '{}' ".format(sys.argv[4])
+    query += "ORDER BY id ASC"
+    cur.execute(query)
 
-rows = cur.fetchall()
-for row in rows:
-    print(row)
+    rows = cur.fetchall()
+    if rows:
+        for row in rows:
+            print(row)
 
-cur.close()
-db.close()
+    cur.close()
+    db.close()
