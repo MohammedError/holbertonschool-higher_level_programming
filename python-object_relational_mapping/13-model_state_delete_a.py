9e69682686fb350d661e93d699364b013c42b90d
MohammedError <mohammed.secure@outlook.sa>
2026-03-02 02:03:43 +0300

    Task 13: Final fix for pycodestyle and permissions

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -14,11 +14,8 @@ if __name__ == "__main__":
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    states_to_delete = session.query(State).filter(
-        State.name.like('%a%')).all()
-    for state in states_to_delete:
+    states = session.query(State).filter(State.name.contains('a'))
+    for state in states:
         session.delete(state)
-
     session.commit()
     session.close()
