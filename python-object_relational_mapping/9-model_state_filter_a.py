f9fec71e119a2c209065c28ef71b75c78619f185
MohammedError <mohammed.secure@outlook.sa>
2026-03-02 01:57:23 +0300

    Fix task 9 permissions and logic

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -10,13 +10,12 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query: states with 'a', sorted by id
-    states = session.query(State).filter(State.name.like('%a%')) \
+    states = session.query(State).filter(State.name.like('%a%'))\
                                  .order_by(State.id).all()
 
     for state in states:
