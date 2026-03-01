cb2e10a343427b72ded0e44ea69de6cf3fc07671
MohammedError <mohammed.secure@outlook.sa>
2026-03-02 00:56:14 +0300

    Fix Task 8: Set permissions and handle empty table

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Prints the first State object from the database hbtn_0e_6_usa
+Lists the first State object from the database hbtn_0e_6_usa
 """
 import sys
 from model_state import Base, State
@@ -11,15 +11,12 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
     first_state = session.query(State).order_by(State.id).first()
-
     if first_state:
         print("{}: {}".format(first_state.id, first_state.name))
     else:
         print("Nothing")
-
     session.close()
