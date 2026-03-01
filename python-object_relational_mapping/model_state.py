cbe2602b247ebcc1080bf754dfc478ea0276a20b
MohammedError <mohammed.secure@outlook.sa>
2026-03-02 00:51:10 +0300

    Task 7: List all states via SQLAlchemy

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Contains the class definition of a State
-and an instance Base = declarative_base()
+Contains the class definition of a State and an instance Base = declarative_base()
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -11,8 +10,7 @@ Base = declarative_base()
 
 class State(Base):
     """
-    State class that inherits from Base
-    Links to the MySQL table states
+    State class that inherits from Base and links to the MySQL table states
     """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
