3a58e39b98cc8c8252317c074ad6062ff5f8a96a
MohammedError <mohammed.secure@outlook.sa>
2026-03-02 00:47:34 +0300

    Fix task 2: Ensure correct indentation and pycodestyle compliance

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
 """
-This script lists all states from the database hbtn_0e_0_usa
-where name matches the argument provided as a parameter.
+This module provides a script that filters states from a database
+based on user input using MySQLdb.
 """
 import MySQLdb
 import sys
 
-if name == "main":
-db = MySQLdb.connect(
-host="localhost",
-port=3306,
-user=sys.argv[1],
-passwd=sys.argv[2],
-db=sys.argv[3],
-charset="utf8"
-)
-cur = db.cursor()
-query = "SELECT * FROM states WHERE BINARY name = '{}' ".format(sys.argv[4])
-query += "ORDER BY id ASC"
-cur.execute(query)
-rows = cur.fetchall()
-for row in rows:
-print(row)
-cur.close()
-db.close()
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' \
+ORDER BY states.id ASC".format(sys.argv[4])
+    cursor.execute(query)
+    query_rows = cursor.fetchall()
+    for row in query_rows:
+        print(row)
+    cursor.close()
+    db.close()
